1f25470dd117f44d5fd14ce28cb1cbda9946ae1f
Lett <lett@mail.lettsn.org>
2026-04-08 20:42:56 -0400

    Method to download asset.

--- src/fetcher.rs
+++ src/fetcher.rs
@@ -1,10 +1,14 @@
-use reqwest::Client;
+use std::fs::File;
+use std::io::{Write};
+use reqwest::blocking::Client;
 use reqwest::header::USER_AGENT;
 use serde::Deserialize;
 
 const RELEASES_URL: &str = "https://api.github.com/repos/anuken/mindustry/releases";
+const AGENT_NAME: &str = "Mindustry Version Manager";
 
 #[derive(Deserialize)]
+#[allow(dead_code)]
 pub struct Release {
     pub url: String,
     pub tag_name: String,
@@ -18,11 +22,11 @@ pub struct Release {
 }
 
 #[derive(Deserialize)]
+#[allow(dead_code)]
 pub struct Asset {
     pub url: String,
     pub name: String,
     pub size: u64,
-    pub digest: String,
     pub created_at: String,
     pub updated_at: String,
     pub browser_download_url: String,
@@ -30,11 +34,11 @@ pub struct Asset {
 
 impl Release {
     // TODO: do not panic.
-    pub fn fetch() -> Release {
+    pub fn fetch_multiple() -> Vec<Release> {
         let client = Client::new();
         let response = client
             .get(RELEASES_URL)
-            .header(USER_AGENT, "Mindustry Version Manager")
+            .header(USER_AGENT, AGENT_NAME)
             .send();
 
         let response_str = match response {
@@ -42,6 +46,25 @@ impl Release {
             Err(err) => panic!("Error: {}", err)
         };
 
-        serde_json::from_str(response_str).expect("Unknown error.")
+        // println!("{}", response_str);
+        serde_json::from_str(&*response_str).expect("Unknown error.")
+    }
+}
+
+impl Asset {
+    pub fn download(&self, mut location: &File) -> Result<(), Option<reqwest::Error>> {
+        let client = Client::new();
+        let response = client
+            .get(&self.browser_download_url)
+            .header(USER_AGENT, AGENT_NAME)
+            .send()?;
+
+        if response.status().is_success() {
+            let data = response.bytes()?;
+            location.write_all(&*data.to_vec()).unwrap();
+            return Ok(());
+        }
+
+        Err(None)
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,47 @@
 mod fetcher;
 
 fn main() {
-    println!("Hello, world!");
+
 }
 
 #[cfg(test)]
 mod tests {
-    // #[test]
-    // fn functional() {
-    //     assert_eq!(2, 2);
-    // }
+    use std::fs::{remove_file, File};
+    use std::os::unix::fs::MetadataExt;
+    use crate::fetcher::Release;
+    #[test]
+    fn get_releases() {
+        let releases = Release::fetch_multiple();
+        assert_ne!(releases[0].url, "");
+    }
+
+    #[test]
+    fn download_release() {
+        const PATH: &str = "./Mindustry.jar";
+
+        println!("Downloading jar...");
+
+        let releases = Release::fetch_multiple();
+        let mut file_asset = None;
+
+        for asset in &releases[0].assets {
+            if asset.name.contains("Mindustry") {
+                file_asset = Some(asset);
+                break;
+            }
+        }
+
+        assert!(file_asset.is_some());
+
+        let file_result = File::create(PATH);
+        assert!(file_result.is_ok());
+
+        let file = file_result.unwrap();
+        let result = file_asset.unwrap().download(&file);
+        assert!(result.is_ok());
+        assert_ne!(file.metadata().unwrap().size(), 0);
+
+        remove_file(PATH).expect("Downloaded successfully, but could not delete file.");
+        println!("Download complete.")
+    }
 }
